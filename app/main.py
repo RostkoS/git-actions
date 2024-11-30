db00bb7e4d8699759a4da873356cab993f500a01
rostkoS <s.rostkovych@gmail.com>
2024-11-30 22:47:31 +0200

    fix lint

--- app/main.py
+++ app/main.py
@@ -6,15 +6,11 @@ def exchange(amount, rate):
     return amount * rate
 
 
-
 def lambda_handler(event, context):
     addition_result = add(5, 3)
     exchange_result = exchange(100, 1.2)
 
     return {
         "statusCode": 200,
-        "body": {
-            "Addition": addition_result,
-            "Currency Exchange": exchange_result
-        }
+        "body": {"Addition": addition_result, "Currency Exchange": exchange_result},
     }
